493bf612b29010a01b1a576d034027775f5cec40
Mathis Ravera <ravera.mathis@gmail.com>
2024-02-15 15:56:57 +0100

    first migration with champignon

---
+++ primo/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Champignon
+
+admin.site.register(Champignon)
---
+++ primo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Champignon(models.Model):
+	name = models.CharField(max_length=200)
+	type = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name + ' ' + self.type
